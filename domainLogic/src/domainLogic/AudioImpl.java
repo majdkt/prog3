fbd9979ef7f9a64c7024180b3143b2db1e53e367
majd <s0582852@htw-berlin.de>
2024-04-29 16:08:01 +0200

    project added after first abgabe

---
+++ domainLogic/src/domainLogic/AudioImpl.java
@@ -0,0 +1,65 @@
+package domainLogic;
+
+import contract.Audio;
+import contract.Tag;
+import contract.Uploader;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.Collection;
+import java.util.List;
+
+ class AudioImpl implements Audio {
+    private long accessCount;
+    private int samplingRate;
+
+     // Constructor to initialize the AudioImpl object with a sampling rate
+     public AudioImpl(int samplingRate) {
+         this.samplingRate = samplingRate;
+     }
+
+    @Override
+    public int getSamplingRate() {
+        // Return the stored sampling rate
+        return this.samplingRate;
+    }
+
+    @Override
+    public String getAddress() {
+        return "";
+    }
+
+    @Override
+    public Collection<Tag> getTags() {
+        return List.of();
+    }
+
+    protected void setAccessCount(long value){
+        this.accessCount=value;
+    }
+
+    @Override
+    public long getAccessCount() {
+        return this.accessCount;
+    }
+
+    @Override
+    public long getSize() {
+        return 0;
+    }
+
+    @Override
+    public Uploader getUploader() {
+        return null;
+    }
+
+    @Override
+    public Duration getAvailability() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getCost() {
+        return null;
+    }
+}
---
+++ domainLogic/src/domainLogic/Manager.java
@@ -0,0 +1,32 @@
+package domainLogic;
+
+import contract.Audio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Manager {
+    private List<AudioImpl> audioList = new ArrayList<>();
+
+    Audio create(int SamplingRate){
+        AudioImpl ai = new AudioImpl(SamplingRate); // Pass the SamplingRate to the AudioImpl constructor
+        audioList.add(ai);
+        return null;
+    }
+
+    public List<Audio> read(){
+        return new ArrayList<>(this.audioList);
+    }
+
+    Audio update(int i){
+        AudioImpl audio = audioList.get(i);
+        long ac = audio.getAccessCount();
+        audio.setAccessCount(ac+1);
+        return audio;
+    }
+
+    boolean delete(Audio audio){
+        return audioList.remove(audio);
+    }
+
+}
