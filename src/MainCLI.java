366286a38394ed1a7461e599e6486ef80116f14b
majd <s0582852@htw-berlin.de>
2024-07-28 16:53:50 +0200

    updated MainCLI

--- src/MainCLI.java
+++ src/MainCLI.java
@@ -1,27 +1,20 @@
-package mains;
-
-import cliPack.Menu;
 import eventSystem.EventDispatcher;
 import eventSystem.listeners.MediaListener;
-import eventSystem.listeners.UploaderListener;
 import domainLogic.Manager;
+import cliPack.Menu;
+import eventSystem.listeners.UploaderListener;
 
 public class MainCLI {
     public static void main(String[] args) {
-        // Initialize Manager
-        Manager manager = new Manager(1000000000); // Example capacity of 1GB
-
-        // Initialize EventDispatcher
         EventDispatcher eventDispatcher = new EventDispatcher();
+        Manager manager = new Manager(1000);
 
         // Add Listeners
         eventDispatcher.addListener(new UploaderListener(manager));
         eventDispatcher.addListener(new MediaListener(manager));
 
-        // Initialize Menu
+        // Start the menu
         Menu menu = new Menu(eventDispatcher);
-
-        // Run Menu
         menu.run();
     }
 }
